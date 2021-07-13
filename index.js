a858699d92d7f8d436a60a62838d45fb1184274e
issac4892 <issac4892@gmail.com>
2021-07-13 14:02:30 +0900

    many feat and fix

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 const {app, BrowserWindow} = require('electron')
+const {autoUpdater} = require('electron-updater')
+
+// Updater Start
+
+// Define Menu
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+// Updater End
 
 function createWindow () {  // 브라우저 창을 생성
     let win = new BrowserWindow({
