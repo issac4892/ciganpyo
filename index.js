70d07757adea3f508fd89b2c96ff137b79954b5c
issac4892 <issac4892@gmail.com>
2021-07-13 13:11:43 +0900

    fix: black border

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ function createWindow () {  // 브라우저 창을 생성
     let win = new BrowserWindow({
       width: 450,
       height: 700,
-      webPreferences: {
-        nodeIntegration: true
-      }
+      center: true,
+      resizable: false
     })
   
     //브라우저창이 읽어 올 파일 위치
     win.loadFile('./index.html')
     win.removeMenu()
-    win.setResizable(false)
   }
   
   app.on('ready', createWindow);
