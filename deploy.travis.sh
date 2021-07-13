895df22ae7766a1a117ad238c63b4cdc1dc46c67
issac4892 <issac4892@gmail.com>
2021-07-13 16:41:37 +0900

    fix: version part

--- deploy.travis.sh
+++ deploy.travis.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_OS_NAME" == osx ]; then
     yarn release
 else
     # deploy on windows and linux
-    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "git config --global user.email "issac4892@gmail.com" && git config --global user.name "issac4892" && npm version patch && yarn install && yarn deploy"
+    docker run --rm -e GH_TOKEN -v "${PWD}":/project -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine /bin/bash -c "git config --global user.email "issac4892@gmail.com" && git config --global user.name "issac4892" && npm version patch && git add package.json && git commit -m "Patch [ci skip]" && git push origin master&& yarn install && yarn deploy"
 fi
